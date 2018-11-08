ed9d4dce18381660796080026e52b403074f1b7c
Laura <laura.r.s87@gmail.com>
2018-11-08 12:49:11 +0100

    random answers

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -11,6 +11,7 @@ export class AboutPage {
 
   questions:Question[];
   questionIndex:number=0;
+  answers:string[]=new Array();
 
   constructor(public navCtrl: NavController, public quiz:QuizProvider) {
     this.getAllQuestions();
@@ -19,7 +20,32 @@ export class AboutPage {
   getCurrentQuestion(){
     return this.questions[this.questionIndex];
   }
+
+  getAnswers(){
+    if(this.answers.length==0){
+      this.answers.push(this.getCurrentQuestion().correct_answer);
+      for(let answer in this.getCurrentQuestion().incorrect_answers){
+        this.answers.push(answer);
+      }    
   
+      this.shuffle(this.answers);
+    }
+    
+    return this.answers;
+  }
+
+   shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
   getAllQuestions(){
     this.quiz.getAllQuestions()
     .then(res => this.questions=res)
